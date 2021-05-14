e94f7f445cc15328f717fb06bcbd3af35ca77547
Jasper <80272820+Code-Jap@users.noreply.github.com>
2021-05-14 19:52:50 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const makePizza = function () {
+    console.log("Make dough");
+    console.log("Pre-heat oven");
+    console.log("Cut ingredients");
+    console.log("Top the dough with the ingredients");
+    console.log("Bake the pizza for 13 minutes on 230 degrees");
+};
+
+
+
+const makeSushi = function () {
+    console.log("Make sushi");
+};
+
+makeSushi ();
+makeSushi ();
+makeSushi ();
+
+makePizza ();
+makePizza ();
